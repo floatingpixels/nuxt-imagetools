09b8d025116f202deb02ca794578b81c09e6d655
Aietes <stefan@standa.de>
2024-05-29 15:59:34 +0200

    Adding nix flake and direnv
    Flake lock

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Development environment for Nuxt.js project with Node.js and TypeScript";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        # pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_22 # Node.js
+            nodePackages.pnpm # PNPM package manager
+            typescript # TypeScript
+          ];
+        };
+      }
+    );
+}
