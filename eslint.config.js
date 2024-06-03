b22c1801d91bc46e7259b417274af1373e0220c2
Aietes <stefan@standa.de>
2024-06-03 11:53:01 +0200

    Switch lint config to flat file

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
-export default [
-  {
-    extends: ['@nuxt/eslint-config', 'prettier'],
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default createConfigForNuxt({})
+  .override('nuxt/vue/rules', {
     rules: {
       'vue/no-setup-props-destructure': 'off',
       'vue/multi-word-component-names': 'off',
     },
-  },
-]
-
+  })
+  .append(eslintConfigPrettier)
+  .append({
+    ignores: [],
+  })
